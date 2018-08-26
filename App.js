7dd62f406225446d727de724ac2fe7be52a79cbb
Didin-Studio <okakzai@gmail.com>
2018-08-26 20:28:38 +0700

    Versi 0.55.4

---
+++ App.js
@@ -0,0 +1,4 @@
+import React from 'react';
+import { StackNavigator } from 'react-navigation';
+import routes from './config/routes';
+export default StackNavigator(routes);
---
+++ config/routes.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import Home from '../screens/Home';
+import ChatScreen from '../screens/ChatScreen';
+const routes = {
+    home: { screen: Home },
+    chat: { screen: ChatScreen }
+}
+export default routes;
